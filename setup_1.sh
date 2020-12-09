84bbe005bdb427971375453f88666e7deb783d9b
Melvin Suter <melvin@suter.dev>
2020-12-09 15:17:48 +0100

    references wrong script

--- setup_1.sh
+++ setup_1.sh
@@ -6,7 +6,7 @@ role_install_name=install_X_graylog.yml
 
 # Setup Ansible
 echo "=== Setting up the ansible project ==="
-curl -s https://raw.githubusercontent.com/uniQconsulting-ag/ansible.os_basis_setup_project/master/install_0_prep_ansible.sh | sh > /dev/null
+curl -s https://raw.githubusercontent.com/uniQconsulting-ag/ansible_setup/master/install_0_prep_ansible.sh | sh > /dev/null
 
 echo "=== Cleaning up ==="
 cd /etc/ansible/projects/uqcSetup > /dev/null
